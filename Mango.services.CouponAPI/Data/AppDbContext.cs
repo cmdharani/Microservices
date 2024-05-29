f7791130771b1039e5d292b6b5771fed690b2e4b
madhu dharani <cmdharani2014@gmail.com>
2024-05-29 12:02:06 +0530

    Creating DB and Seeding the table

---
+++ Mango.services.CouponAPI/Data/AppDbContext.cs
@@ -0,0 +1,52 @@
+﻿using Mango.services.CouponAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.services.CouponAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+
+        public DbSet<Coupon> Coupons { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Coupon>().HasData(
+                new Coupon()
+                {
+                    CouponId = 1,
+                    CouponCode = "10OFF",
+                    DiscountAmount = 10,
+                    MinAmount = 20
+                }
+                );
+
+            modelBuilder.Entity<Coupon>().HasData(
+               new Coupon()
+               {
+                   CouponId = 2,
+                   CouponCode = "20OFF",
+                   DiscountAmount = 20,
+                   MinAmount = 20
+               }
+               );
+
+            modelBuilder.Entity<Coupon>().HasData(
+               new Coupon()
+               {
+                   CouponId = 3,
+                   CouponCode = "30OFF",
+                   DiscountAmount = 30,
+                   MinAmount = 40
+               }
+               );
+        }
+
+    }
+}
--- Mango.services.CouponAPI/Models/Coupon.cs
+++ Mango.services.CouponAPI/Models/Coupon.cs
@@ -1,10 +1,16 @@
-﻿namespace Mango.services.CouponAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mango.services.CouponAPI.Models
 {
     public class Coupon
     {
+        [Key]
         public int CouponId { get; set; }
-        public string CouponCode { get; set; }
+        [Required]
+        public string CouponCode { get; set; }=string.Empty;
+        [Required]
         public double DiscountAmount { get; set; }
+        [Required]
         public int  MinAmount { get; set; }
 
     }
