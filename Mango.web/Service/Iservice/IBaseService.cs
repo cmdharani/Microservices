b9d1a97332bf1b1e39b25ae0773d8c3c1f77f7af
madhu dharani <cmdharani2014@gmail.com>
2024-05-29 20:11:06 +0530

    Base and Coupon Service Implementation

---
+++ Mango.web/Service/Iservice/IBaseService.cs
@@ -0,0 +1,9 @@
+﻿using Mango.web.Models;
+
+namespace Mango.web.Service.Iservice
+{
+    public interface IBaseService
+    {
+      Task< ResponseDto?>  sendAsync(RequestDto requestDto);
+    }
+}
---
+++ Mango.web/Service/Iservice/ICouponService.cs
@@ -0,0 +1,16 @@
+﻿using Mango.web.Models;
+
+namespace Mango.web.Service.Iservice
+{
+    public interface ICouponService
+    {
+        Task<ResponseDto?> GetCouponAsync(string couponcode);
+        Task<ResponseDto?> GetAllCouponAsync();
+        Task<ResponseDto?> GetCouponByIdAsync(int id);
+        Task<ResponseDto?> CreateCouponAsync(CouponDto couponDto);
+        Task<ResponseDto?> UpdateCouponAsync(CouponDto couponDto);
+        Task<ResponseDto?> DeleteCouponAsync(int id);
+
+
+    }
+}
