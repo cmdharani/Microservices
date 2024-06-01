83a35bee35b222b62a24dc0e057f39a3e7627fe7
madhu dharani <cmdharani2014@gmail.com>
2024-06-01 13:20:54 +0530

    JWT token generation service

---
+++ Mango.services.AuthAPI/Service/IService/IJwtTokenGenerator.cs
@@ -0,0 +1,10 @@
+﻿using Mango.services.AuthAPI.Migrations;
+using Mango.Services.AuthAPI.Models;
+
+namespace Mango.services.AuthAPI.Service.IService
+{
+    public interface IJwtTokenGenerator
+    {
+        string GenerateToken(ApplicationUser applicationUser);
+    }
+}
---
+++ Mango.services.AuthAPI/Service/JwtTokenGenerator.cs
@@ -0,0 +1,55 @@
+﻿using Mango.services.AuthAPI.Migrations;
+using Mango.services.AuthAPI.Service.IService;
+using Mango.Services.AuthAPI.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.Data;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Mango.services.AuthAPI.Service
+{
+    public class JwtTokenGenerator : IJwtTokenGenerator
+    {
+        private readonly JwtOptions _jwtOptions;
+
+        public JwtTokenGenerator(JwtOptions jwtOptions)
+        {
+            _jwtOptions = jwtOptions;
+        }
+
+
+
+        public string GenerateToken(ApplicationUser applicationUser)
+        {
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var key = Encoding.ASCII.GetBytes(_jwtOptions.Secret);
+
+            var claimList = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Email,applicationUser.Email),
+                new Claim(JwtRegisteredClaimNames.Sub,applicationUser.Id),
+                new Claim(JwtRegisteredClaimNames.Name,applicationUser.UserName)
+            };
+
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Audience = _jwtOptions.Audience,
+                Issuer = _jwtOptions.Issuer,
+                Subject = new ClaimsIdentity(claimList),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+
+
+
+
+        }
+    }
+}
