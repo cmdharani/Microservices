f5a64f6fd2c84ec70f149994d247c2a10f041090
madhu dharani <cmdharani2014@gmail.com>
2024-06-03 07:11:41 +0530

    Adding DTOs to the web Project for the consuming the Auth service

---
+++ Mango.web/Models/LoginRequestDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Mango.web.Models
+{
+    public class LoginRequestDto
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
