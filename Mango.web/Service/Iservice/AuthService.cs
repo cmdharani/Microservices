dea51179dbde9a5505072b373ace23a2b4b58495
madhu dharani <cmdharani2014@gmail.com>
2024-06-03 07:35:21 +0530

    Auth service

---
+++ Mango.web/Service/Iservice/AuthService.cs
@@ -0,0 +1,49 @@
+﻿using Mango.web.Models;
+using Mango.web.Utility;
+
+namespace Mango.web.Service.Iservice
+{
+    public class AuthService : IAuthService
+    {
+
+        private readonly IBaseService _baseService;
+
+        public AuthService(IBaseService baseService)
+        {
+            _baseService = baseService;
+        }
+
+        public async Task<ResponseDto?> AssignRoleAsync(RegistrationRequestDto registrationRequestDto)
+        {
+            return await _baseService.sendAsync(new RequestDto()
+            {
+                ApiType = SD.ApiType.POST,
+                Data = registrationRequestDto,
+                Url = SD.AuthAPIBase + "/api/auth/AssignRole"
+
+            });
+        }
+
+        public async Task<ResponseDto?> LoginAsync(LoginRequestDto loginRequestDto)
+        {
+            return await _baseService.sendAsync(new RequestDto()
+            {
+                ApiType = SD.ApiType.POST,
+                Data = loginRequestDto,
+                Url = SD.AuthAPIBase + "/api/auth/login"
+
+            });
+        }
+
+        public async Task<ResponseDto?> RegisterAsync(RegistrationRequestDto registrationRequestDto)
+        {
+            return await _baseService.sendAsync(new RequestDto()
+            {
+                ApiType = SD.ApiType.POST,
+                Data = registrationRequestDto,
+                Url = SD.AuthAPIBase + "/api/auth/register"
+
+            });
+        }
+    }
+}
