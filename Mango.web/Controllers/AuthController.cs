4be8dd3a20bc1e49a9632fbacd6701a42b7c268f
madhu dharani <cmdharani2014@gmail.com>
2024-06-03 17:48:51 +0530

    adding Token

--- Mango.web/Controllers/AuthController.cs
+++ Mango.web/Controllers/AuthController.cs
@@ -1,19 +1,27 @@
 ﻿using Mango.web.Models;
+using Mango.web.Service;
 using Mango.web.Service.Iservice;
 using Mango.web.Utility;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 
 namespace Mango.web.Controllers
 {
     public class AuthController : Controller
     {
         private readonly IAuthService authService;
+        private readonly ITokenProvider tokenProvider;
 
-        public AuthController(IAuthService _authService)
+
+        public AuthController(IAuthService _authService, ITokenProvider _tokenProvider)
         {
             authService = _authService;
+          tokenProvider = _tokenProvider;
         }
 
         [HttpGet]
@@ -34,7 +42,11 @@ namespace Mango.web.Controllers
 
             if (responseDto != null && responseDto.isSuccess)
             {
-                LoginResponseDto loginRequestDto = JsonConvert.DeserializeObject<LoginResponseDto>(Convert.ToString(responseDto.Result));
+                LoginResponseDto loginResponseDto = JsonConvert.DeserializeObject<LoginResponseDto>(Convert.ToString(responseDto.Result));
+
+                tokenProvider.SetToken(loginResponseDto.Token);
+
+                await SignInUser(loginResponseDto);
                 return  RedirectToAction("Index","Home");
 
             }
@@ -102,10 +114,40 @@ namespace Mango.web.Controllers
         }
 
 
-        public IActionResult Logout()
+        public async Task<IActionResult> Logout()
         {
-            return View();
+            await HttpContext.SignOutAsync();
+            tokenProvider.ClearToken();
+            return RedirectToAction("Index", "Home");
         }
+
+
+        private async Task SignInUser(LoginResponseDto model)
+        {
+            var handler = new JwtSecurityTokenHandler();
+
+            var jwt = handler.ReadJwtToken(model.Token);
+
+            var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme);
+            identity.AddClaim(new Claim(JwtRegisteredClaimNames.Email,
+                jwt.Claims.FirstOrDefault(u => u.Type == JwtRegisteredClaimNames.Email).Value));
+            identity.AddClaim(new Claim(JwtRegisteredClaimNames.Sub,
+                jwt.Claims.FirstOrDefault(u => u.Type == JwtRegisteredClaimNames.Sub).Value));
+            identity.AddClaim(new Claim(JwtRegisteredClaimNames.Name,
+                jwt.Claims.FirstOrDefault(u => u.Type == JwtRegisteredClaimNames.Name).Value));
+
+
+            identity.AddClaim(new Claim(ClaimTypes.Name,
+                jwt.Claims.FirstOrDefault(u => u.Type == JwtRegisteredClaimNames.Email).Value));
+            //identity.AddClaim(new Claim(ClaimTypes.Role,
+            //    jwt.Claims.FirstOrDefault(u => u.Type == "role").Value));
+
+
+
+            var principal = new ClaimsPrincipal(identity);
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+        }
+
 
     }
 }
