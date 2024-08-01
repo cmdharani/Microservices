61f73ec906ecf6ed152c3f3e115d66829e168f38
madhu dharani <cmdharani@outlook.com>
2024-08-01 23:11:28 +0530

    Coconfigure JWT Options

--- Mango.Services.AuthAPINew/Program.cs
+++ Mango.Services.AuthAPINew/Program.cs
@@ -9,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-
+builder.Services.Configure<JwtOptions>(builder.Configuration.GetSection("ApiSettings:JwtOptions"));
 
 builder.Services.AddDbContext<AppDbContext>(
     options=>options.UseSqlServer(builder.Configuration.GetConnectionString("defaultConnection"))
